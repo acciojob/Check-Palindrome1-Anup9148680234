f0964782182f05d8389e7ac912bc22929352b86a
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-03-26 19:53:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.replaceAll(" ","");
+	var reve = str.split('').reverse().join('');
+	if(str == reve){
+		return true;
+	}
+	return false;
 }
 module.exports = palindrome
